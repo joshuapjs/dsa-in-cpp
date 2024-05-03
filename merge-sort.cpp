5e293486028da54cdcdb1f575605a97a88cfb02c
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-05-04 00:51:44 +0200

    first session

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -2,10 +2,38 @@
 // Author: Joshua Spingler
 // Date: 02.05.2024 
 
-# include <cstdio>
+# include <vector>
+# include <iostream>
+using namespace std;
+
+vector<int> merge_sort(vector<int> v);
 
 int main {
-		
 
-	return 1
+	vector<int> test_array{5, 7, 1, 28, 11, 2, 6, 4};
+	int merge_sort(test_array);
+
+	return 0;
+}
+
+vector<int> merge_sort(vector<int> v) {
+	int vector_size = v.size();
+	vector<int> output_v;
+
+	if (vector_size = 2) {
+		if (v.front() > v.back()) {
+			vector<int> output_v{v.back(), v.front()};
+			return output_v;
+		}
+		else {
+			vector<int> output_v{v.front(), v.back()};
+			return output_v;
+		}
+	}
+	else {
+		int new_index = vector_size / 2;
+		merge_sort(vector<int> v(v.begin(), v.begin() + vector_size));
+		merge_sort(vector<int> v(v.begin() + vector_size, v.end()));
+	}
+
 }
