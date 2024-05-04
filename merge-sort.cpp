a5361b60a1e180a341029122e48125f011061b10
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-05-04 23:54:09 +0200

    adjusted indentation

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -4,36 +4,28 @@
 
 # include <vector>
 # include <iostream>
+
 using namespace std;
 
 vector<int> merge_sort(vector<int> v);
 
 int main {
+    vector<int> test_array{5, 7, 1, 28, 11, 2, 6, 4};
+    int merge_sort(test_array);
 
-	vector<int> test_array{5, 7, 1, 28, 11, 2, 6, 4};
-	int merge_sort(test_array);
-
-	return 0;
+    return 0;
 }
 
 vector<int> merge_sort(vector<int> v) {
-	int vector_size = v.size();
-	vector<int> output_v;
-
-	if (vector_size = 2) {
-		if (v.front() > v.back()) {
-			vector<int> output_v{v.back(), v.front()};
-			return output_v;
-		}
-		else {
-			vector<int> output_v{v.front(), v.back()};
-			return output_v;
-		}
-	}
-	else {
-		int new_index = vector_size / 2;
-		merge_sort(vector<int> v(v.begin(), v.begin() + vector_size));
-		merge_sort(vector<int> v(v.begin() + vector_size, v.end()));
-	}
-
+    int vector_size = v.size();
+    if (vector_size = 2) {
+        if (v[0] > v[1]) {
+            vector<int> swap(v[0],v[1]);
+        }
+    }
+    else {
+        int new_index = vector_size / 2;
+        merge_sort(vector<int> v(v.begin(), v.begin() + vector_size));
+        merge_sort(vector<int> v(v.begin() + vector_size, v.end()));
+    }
 }
