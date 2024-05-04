91c54e70f7e96f0dcd609caa685338a5738237e6
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-05-05 01:53:15 +0200

    update - not finished

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -4,28 +4,58 @@
 
 # include <vector>
 # include <iostream>
-
 using namespace std;
 
 vector<int> merge_sort(vector<int> v);
 
-int main {
+int main() {
     vector<int> test_array{5, 7, 1, 28, 11, 2, 6, 4};
-    int merge_sort(test_array);
+    vector<int> output = merge_sort(test_array);
+    for (const auto& element : output) {
+    cout << element << " ";
+    }
+    cout << std::endl;
 
     return 0;
 }
 
 vector<int> merge_sort(vector<int> v) {
     int vector_size = v.size();
-    if (vector_size = 2) {
-        if (v[0] > v[1]) {
-            vector<int> swap(v[0],v[1]);
-        }
+    if (vector_size == 2 && v[0] > v[1]) {
+        swap(v[1],v[0]);
+        return v;
     }
+    else if (vector_size > 2) {
+        vector<int> next_v_1 = vector<int>(v.begin(), v.begin() + vector_size / 2);
+        vector<int> next_v_2 = vector<int>(v.begin() + vector_size / 2, v.end());
+        vector<int> v_1 = merge_sort(next_v_1);
+        vector<int> v_2 = merge_sort(next_v_2);
+
+        vector<int> output_v;
+
+        while ((not v_1.empty()) && (not v_2.empty())) {
+            if (v_1.front() < v_2.front()) {
+                output_v.push_back(v_1.front());
+                v_1.erase(v_1.begin());
+            }
+            else {
+                output_v.push_back(v_2.front());
+                v_2.erase(v_2.begin());
+            }
+        }
+        if (not v_1.empty()) {
+            int v_1_size = v_1.size();
+            for (int i; i < v_1_size; i++) {
+                output_v.push_back(v_1[i]);
+            }}
+        else if (not v_2.empty()) {
+            int v_2_size = v_2.size();
+            for (int i; i < v_2_size; i++) {
+                output_v.push_back(v_2[i]);
+            }
+        }
+        return output_v;}
     else {
-        int new_index = vector_size / 2;
-        merge_sort(vector<int> v(v.begin(), v.begin() + vector_size));
-        merge_sort(vector<int> v(v.begin() + vector_size, v.end()));
+        return v;
     }
 }
