e8148c8c71ab9ff96cf204eb08a9c2806bfda523
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-10-03 19:54:16 +0200

    Separation of heap in heap.h and heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,202 +1,191 @@
-/*this is an exploratory implementation of a heap in c++.
-* every step is documented in a vary detailed manner as it is intended for educational purposes.
+/*This is an implementation file of heap.h
+* Every step is documented vary detailed as it is intended for educational purposes.
 *
-* author: joshua spingler
-* date: 30.08.2024
+* Author: Joshua Spingler
+* Date: 30.08.2024
 */
 
+#include "heap.h"
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
 
-class Heap {
 	
-	// Declaring the typical vector to store the values of the Heap.
-	vector<int> data_array;
-
-	// We declare repair_up(wards) and repair_down(wards) to repair the heap-order if it is violated.
+// We declare repair_up(wards) and repair_down(wards) to repair the heap-order if it is violated.
 	
-	void repair_up(int index) {
-		// The vector size must be bigger than 1 (otherwise there is no need to assign a parent to the vertex).
-		// The index must be above 0 as we can't call repair_up on the root of the heap.
-		// The index must be below data_array.size() otherwise there will be undefined behaviour as there is 1-Indexing.
-		if ((data_array.size() > 1) && 
-				(index > 0) && 
-				(index <= data_array.size() - 1)) {
+void Heap::repair_up(int index) {
+	// The vector size must be bigger than 1 (otherwise there is no need to assign a parent to the vertex).
+	// The index must be above 0 as we can't call repair_up on the root of the heap.
+	// The index must be below data_array.size() otherwise there will be undefined behaviour as there is 1-Indexing.
+	if ((data_array.size() > 1) && 
+			(index > 0) && 
+			(index <= data_array.size() - 1)) {
 
-			// The index must be increased by one for the calculation of the father vertex.
-			// The current parent of the vertex in question has index / 2 in the definition I'm using.
-			int father = ((index + 1) / 2)  - 1;
+		// The index must be increased by one for the calculation of the father vertex.
+		// The current parent of the vertex in question has index / 2 in the definition I'm using.
+		int father = ((index + 1) / 2)  - 1;
 
-			// This must be changed if the heap should be transformed in a min Heap.
-			// In the standard implementation repair_up will bring a vertex higher in the tree until the structure
-			// is repaired (the father must have an equal or higher value than the vertex repaired. If not, they have to switch places.
-			while (data_array[father] < data_array[index]) {
-				// This will swap the values stored on the two addresses within the vector and is similar to calling swap(*vertex_a_pointer, *vertex_b_pointer).
-				iter_swap(data_array.begin() + index, 
-						data_array.begin() + father);
-				// Now as the value was assigned to the father vertex we have to update the index to the index of the father vertex.
-				index = father;
-				// And calculate the index of the father vertex.
-				father = ((index + 1) / 2) - 1;
+		// This must be changed if the heap should be transformed in a min Heap.
+		// In the standard implementation repair_up will bring a vertex higher in the tree until the structure
+		// is repaired (the father must have an equal or higher value than the vertex repaired. If not, they have to switch places.
+		while (data_array[father] < data_array[index]) {
+			// This will swap the values stored on the two addresses within the vector and is similar to calling swap(*vertex_a_pointer, *vertex_b_pointer).
+			iter_swap(data_array.begin() + index, 
+					data_array.begin() + father);
+			// Now as the value was assigned to the father vertex we have to update the index to the index of the father vertex.
+			index = father;
+			// And calculate the index of the father vertex.
+			father = ((index + 1) / 2) - 1;
+		}
+	}
+}
+
+void Heap::repair_down(int index) {
+	// The vector size must be bigger than 1 (otherwise there is no need to assign a parent to the vertex).
+	// The index must be above 0 as we can't call repair_up on the root of the heap.
+	// The index must be below data_array.size() otherwise there will be undefined behaviour as there is 1-Indexing.
+	if ((data_array.size() > 1) && 
+			(index >= 0) && 
+			(index < data_array.size() - 1)) {
+
+		// Calculate the indices of potential child vertecies and from the last index.
+		int possible_child_index_a = ((index + 1) * 2) - 1;
+		int possible_child_index_b = possible_child_index_a + 1;
+		int last_possible_index = data_array.size() - 1;
+
+		// Decision if child vertecies exist.
+		// If not the value of the child will be set equal to the current index and thereby never be 
+		// bigger or smaller that the current index. This will falsify the while loop if both indices are 
+		int child_index_a = possible_child_index_a <= last_possible_index ? possible_child_index_a : index;
+		int child_index_b = possible_child_index_b <= last_possible_index ? possible_child_index_b : index;
+
+		// Swap values in the Vertex as explained in repair_up but downwards as long as the Heap-Order is not restored
+		// and as long as there are values to swap.
+		while ((data_array[index] < data_array[child_index_a]) ||
+				(data_array[index] < data_array[child_index_b])) {
+
+			if (data_array[child_index_a] > data_array[child_index_b]) {
+				iter_swap(data_array.begin() + index,
+						data_array.begin() + child_index_a);
+				index = child_index_a;
 			}
+			else {
+				iter_swap(data_array.begin() + index,
+						data_array.begin() + child_index_b);
+				index = child_index_b;
+			}
+
+			// Recalculate the indices for the next children.
+			possible_child_index_a = ((index + 1) * 2) - 1;
+			possible_child_index_b = possible_child_index_a + 1;
+
+			// Get the priority of the children if they exist.
+			child_index_a = possible_child_index_a <= last_possible_index ? possible_child_index_a : index;
+			child_index_b = possible_child_index_b <= last_possible_index ? possible_child_index_b : index;
 		}
 	}
 
-	void repair_down(int index) {
-		// The vector size must be bigger than 1 (otherwise there is no need to assign a parent to the vertex).
-		// The index must be above 0 as we can't call repair_up on the root of the heap.
-		// The index must be below data_array.size() otherwise there will be undefined behaviour as there is 1-Indexing.
-		if ((data_array.size() > 1) && 
-				(index >= 0) && 
-				(index < data_array.size() - 1)) {
+}
 
-			// Calculate the indices of potential child vertecies and from the last index.
-			int possible_child_index_a = ((index + 1) * 2) - 1;
-			int possible_child_index_b = possible_child_index_a + 1;
-			int last_possible_index = data_array.size() - 1;
+// Constructor
+Heap::Heap(vector<int> start_vector) : data_array(start_vector) {}
 
-			// Decision if child vertecies exist.
-			// If not the value of the child will be set equal to the current index and thereby never be 
-			// bigger or smaller that the current index. This will falsify the while loop if both indices are 
-			int child_index_a = possible_child_index_a <= last_possible_index ? possible_child_index_a : index;
-			int child_index_b = possible_child_index_b <= last_possible_index ? possible_child_index_b : index;
+// Destructor
+Heap::~Heap() {
+	// When the Heap is destructed the memory of the arrays must be released.
+	data_array.clear();
+}
 
-			// Swap values in the Vertex as explained in repair_up but downwards as long as the Heap-Order is not restored
-			// and as long as there are values to swap.
-			while ((data_array[index] < data_array[child_index_a]) ||
-					(data_array[index] < data_array[child_index_b])) {
+// Copy Constructor
+Heap::Heap(const Heap& other) : data_array( other.data_array ) {}
 
-				if (data_array[child_index_a] > data_array[child_index_b]) {
-					iter_swap(data_array.begin() + index,
-							data_array.begin() + child_index_a);
-					index = child_index_a;
-				}
-				else {
-					iter_swap(data_array.begin() + index,
-							data_array.begin() + child_index_b);
-					index = child_index_b;
-				}
+// Copy Assignment Operator
+Heap&  Heap::operator=(const Heap& other) {
+	if (this == &other) return *this;
+	data_array = other.data_array;
+	return *this;
+}
 
-				// Recalculate the indices for the next children.
-				possible_child_index_a = ((index + 1) * 2) - 1;
-				possible_child_index_b = possible_child_index_a + 1;
+// Move Constructor
+Heap::Heap(Heap&& other) noexcept : data_array( other.data_array ) {
+	other.data_array.clear();
+}
 
-				// Get the priority of the children if they exist.
-				child_index_a = possible_child_index_a <= last_possible_index ? possible_child_index_a : index;
-				child_index_b = possible_child_index_b <= last_possible_index ? possible_child_index_b : index;
-			}
-		}
+// Move Assignment Operator
+Heap& Heap::operator=(Heap&& other) noexcept {
+	if (this == &other)  return *this;
+	data_array = other.data_array;
+	other.data_array.clear();
+	return *this;
+}
 
-	}
+vector<int> Heap::read() const {
+	return data_array;
+}
 
-	public:
-	
-	// Constructor
-	Heap(vector<int> start_array) : data_array(start_array) {}
+void Heap::add(int element) {
+	// push_back adds an element at the End of the veoctor in increases the size if necessary.
+	data_array.push_back(element);
+}
 
-	// Destructor
-	~Heap() {
-		// When the Heap is destructed the memory of the arrays must be released.
-		data_array.clear();
-	}
+void Heap::remove(int index){
+	// we remove by a similar technique as delete_max by changing the priority of the last Element and deleting the last priority.
+	int last_priority = data_array.back();
+	data_array.pop_back();
+	change_priority(index, last_priority);
+}
 
-	// Copy Constructor
-	Heap(const Heap& other) : data_array( other.data_array ) {}
+int Heap::delete_max() {
+	// Removes the Vertex with the highest priority and returns the Priority.
+	// Then to repair the Heap-Order the last Element in the Heap has to be assigned to the root 
+	// so that we can call repair_down on it.
 
-	// Copy Assignment Operator
-	Heap&  operator=(const Heap& other) {
-		if (this == &other) return *this;
-		data_array = other.data_array;
-		return *this;
-	}
+	// We separate different cases of the vector.size() if there is more than one value the Heap-Structure and Order
+	// must be restored again.
+	if (data_array.size() > 1) {
+		int max = data_array[0];
+		data_array[0] = data_array.back();
+		repair_down(0);
+		return max;
 
-	// Move Constructor
-	Heap(Heap&& other) noexcept : data_array( other.data_array ) {
-		other.data_array.clear();
-	}
-
-	// Move Assignment Operator
-	Heap& operator=(Heap&& other) noexcept {
-		if (this == &other)  return *this;
-		data_array = other.data_array;
-		other.data_array.clear();
-		return *this;
-	}
-	
-	vector<int> read() {
-		return data_array;
-	}
-
-	void add(int data_point) {
-		// push_back adds an element at the End of the veoctor in increases the size if necessary.
-		data_array.push_back(data_point);
-	}
-
-	void remove(int index){
-		// we remove by a similar technique as delete_max by changing the priority of the last Element and deleting the last priority.
-		int last_priority = data_array.back();
+	// if there is only one value we don't need to repair anything.
+	} else if (data_array.size() == 1) {
+		int max = data_array[0];
 		data_array.pop_back();
-		change_priority(index, last_priority);
+		return max;
+
+	// In the last case the Heap has no values and nothing can be returned.
+	} else {
+		cout << "No values in the Heap" << endl;
+		return 0;
 	}
+}
 
-	int delete_max() {
-		// Removes the Vertex with the highest priority and returns the Priority.
-		// Then to repair the Heap-Order the last Element in the Heap has to be assigned to the root 
-		// so that we can call repair_down on it.
-
-		// We separate different cases of the vector.size() if there is more than one value the Heap-Structure and Order
-		// must be restored again.
-		if (data_array.size() > 1) {
-			int max = data_array[0];
-			data_array[0] = data_array.back();
-			repair_down(0);
-			return max;
-
-		// if there is only one value we don't need to repair anything.
-		} else if (data_array.size() == 1) {
-			int max = data_array[0];
-			data_array.pop_back();
-			return max;
-
-		// In the last case the Heap has no values and nothing can be returned.
-		} else {
-			cout << "No values in the Heap" << endl;
-			return 0;
-		}
+int Heap::change_priority(int index, int new_priority) {
+	// If the priority of one vertex changed we have to update the current priority to the new priority and repair the Heap Order.
+	int old_priority = data_array[index];
+	data_array[index] = new_priority;
+	// We decide if repair_up or repair_down has to be called based on the value of the old_priority.
+	if (new_priority < old_priority) {
+		repair_down(index);
+		return 1;
+	} else if (new_priority > old_priority) {
+		repair_up(index);
+		return 1;
+	} else {
+		return 0;
 	}
+}  
 
-	int change_priority(int index, int new_priority) {
-		// If the priority of one vertex changed we have to update the current priority to the new priority and repair the Heap Order.
-		int old_priority = data_array[index];
-		data_array[index] = new_priority;
-		// We decide if repair_up or repair_down has to be called based on the value of the old_priority.
-		if (new_priority < old_priority) {
-			repair_down(index);
-			return 1;
-		} else if (new_priority > old_priority) {
-			repair_up(index);
-			return 1;
-		} else {
-			return 0;
-		}
-	}  
-
-	void build_heap() {
-		// To build the Heap from scratch in case an array was assigned to the Heap, we start at the last vertex that is not a Leaf,
-		// then we call repair_down on each vertex as with each level of depth that was repaired the Heap Order is restored for this level and all levels below.
-		int end_index = data_array.size() - 1;
-		int index = ((end_index + 1)/ 2) - 1;
-		while (index >=0) {
-			repair_down(index);
-			index--;
-		}
+void Heap::build_heap() {
+	// To build the Heap from scratch in case an array was assigned to the Heap, we start at the last vertex that is not a Leaf,
+	// then we call repair_down on each vertex as with each level of depth that was repaired the Heap Order is restored for this level and all levels below.
+	int end_index = data_array.size() - 1;
+	int index = ((end_index + 1)/ 2) - 1;
+	while (index >=0) {
+		repair_down(index);
+		index--;
 	}
-
-	};
-
-int main() {
-	return 0;
-	}
-
+}
---
+++ heap.h
@@ -0,0 +1,32 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Heap {
+	
+	// Declaring the typical vector to store the values of the Heap.
+	vector<int> data_array;
+	void repair_up(int index);
+	void repair_down(int index);
+
+	public:
+
+	Heap(vector<int> start_vector);
+	~Heap();
+	Heap(const Heap& other);
+	Heap&  operator=(const Heap& other);
+	Heap(Heap&& other) noexcept;
+	Heap& operator=(Heap&& other) noexcept;
+	vector<int> read() const;
+	void add(int element);
+	void remove(int index);
+	int delete_max();
+	int change_priority(int index, int new_priority);
+	void build_heap();
+};
+
+#endif
